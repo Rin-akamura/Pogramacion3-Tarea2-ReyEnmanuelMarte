51edcc4bef19d674bb2ad96c0910ac697abb5575
Rey Enmanuel Marte Picherdo <email>
2026-05-22 21:25:50 -0400

    Creacion de la clase Main y estructuracion del menu del Registro de vehiculos

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Main{
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        int op = 0;
+
+        do{
+            System.out.println("=========================================");
+            System.out.println("        .::REGISTRO DE VEHICULOS::.      ");
+            System.out.println("=========================================");
+
+            System.out.println("-----------------------------------------");
+            System.out.println("1. Registrar Vehiculo");
+            System.out.println("2. Mostrar Vehiculos");
+            System.out.println("3. Buscar vehiculo por placa");
+            System.out.println("4. Mostrar vehiculos por marca");
+            System.out.println("5. Salir");
+            System.out.println("-----------------------------------------");
+
+            System.out.print("Elija la opcion deseada: ");
+            op = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(op){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Opcion invalida. Vuelva a intentarlo");
+                    break;
+            }
+        }while(op != 5);
+    }
+}
